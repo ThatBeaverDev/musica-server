df107c628929b2df6ff595ada83d30df2ce58d3b
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-09-01 12:14:25 +0100

    fix(frontend): Migrate BigPlayButton to use a CSS file

--- src/client/components/BigPlayButton.tsx
+++ src/client/components/BigPlayButton.tsx
@@ -1,5 +1,5 @@
-import { useState } from "preact/hooks";
 import React from "react";
+import styles from "./BigPlayButton.module.css";
 
 export default function BigPlayButton({
 	onClick,
@@ -10,69 +10,16 @@ export default function BigPlayButton({
 	style?: React.HTMLAttributes<HTMLDivElement>["style"];
 	onClick: () => void;
 }) {
-	const [hovered, setHovered] = useState(false);
-	const [active, setActive] = useState(false);
-
 	return (
 		<div
-			style={{ ...style, ...styles.outer(hovered, active) }}
-			className={className}
-
-			onMouseEnter={() => setHovered(true)}
-			onMouseLeave={() => {
-				setHovered(false);
-				setActive(false);
-			}}
-
-			onMouseDown={() => setActive(true)}
-			onMouseUp={() => setActive(false)}
+			style={style}
+			className={`${styles.outer} ${className}`}
 
 			onClick={() => onClick()}
 		>
-			<img src="/img/play.svg" style={styles.icon} />
+			<img src="/img/play.svg" className={styles.icon} />
 
-			<p style={styles.text}>Play</p>
+			<p className={styles.text}>Play</p>
 		</div>
 	);
 }
-
-const styles = {
-	outer(hovered: boolean, active: boolean) {
-		return {
-			display: "flex",
-			flexDirection: "row" as "row",
-			alignItems: "center" as "center",
-			gap: "1rem",
-
-			borderRadius: "100px",
-			backgroundColor: "white",
-
-			width: "max-content",
-
-			padding: "10px",
-			paddingLeft: "2rem",
-			paddingRight: "2rem",
-
-			marginTop: "1rem",
-
-			transform: hovered ? (active ? "scale(0.95)" : "scale(1.1)") : "",
-			transition: "ease 0.2s",
-
-			boxShadow: "0 8px 24px rgba(0, 0, 0, 0.4)"
-		};
-	},
-
-	icon: {
-		width: "20px",
-		height: "20px",
-		userSelect: "none" as "none",
-		pointerEvents: "none" as "none"
-	},
-
-	text: {
-		color: "black",
-		verticalAlign: "center",
-		userSelect: "none" as "none",
-		pointerEvents: "none" as "none"
-	}
-};
