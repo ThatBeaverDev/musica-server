cde36e724be31a255f97b4d30e5258538519c77f
ThatBeaverDev <107750535+ThatBeaverDev@users.noreply.github.com>
2026-09-06 10:56:38 +0100

    feat(backend): Log HTTP requests via standard chi middleware

--- src/web/server.go
+++ src/web/server.go
@@ -26,6 +26,7 @@ type WebServer struct {
 func New(idx *indexer.Indexer, idStorage *identityStorage.IdentityStorage, scores *scores.ScoreManager) *WebServer {
 	r := chi.NewRouter()
 	r.Use(middleware.Compress(5))
+	r.Use(middleware.Logger)
 
 	ws := &WebServer{
 		indexer: idx,
